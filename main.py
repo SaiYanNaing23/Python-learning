0e51d4147fca3c2f1f94924ae1f04faf7b575287
Saiyannaing23 <pesilay2372002@gmail.com>
2023-05-17 14:41:00 +0630

    Python Learning

---
+++ main.py
@@ -0,0 +1,12 @@
+name = input("Name : "); 
+print("Hello "+ name); 
+
+age = input("Tell me your current age : ")
+print("your age is " + age) 
+
+# area calculator (pi r **2)
+
+PI = 3.14; 
+r = int(input("radius : "))
+calcArea = PI * r**2; 
+print(calcArea); 
