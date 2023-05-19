8a20b0816c7f77893231f71faaf590efaa43e69d
Saiyannaing23 <pesilay2372002@gmail.com>
2023-05-20 00:48:39 +0630

    Format_string

---
+++ format_string.py
@@ -0,0 +1,8 @@
+age = 20; 
+print("My age is " + str(age))
+
+print("Your age is",age); 
+
+print("My age is",age,"and my birthYear is",2023-age); 
+
+print(f"My age is {age} and my birthyear is {2022-age}"); 
---
+++ input.py
@@ -0,0 +1,24 @@
+# name = input("Name : ");
+# print("Hello "+ name);
+
+# age = input("Tell me your current age : ")
+# print("your age is " + age)
+
+# area calculator (pi r **2)
+
+# PI = 3.14;
+# r = int(input("radius : "))
+# calcArea = PI * r**2;
+# print(calcArea);
+
+
+print("Hello World"); 
+print(input("Name :"))
+
+age = input("Tell me your age : "); 
+print("Your age is " + age)
+
+PI = 3.14; 
+r = int(input("radius: ")); 
+calcArea = PI *r**2; 
+print(calcArea) ; 
