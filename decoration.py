00f83a495f698f4d4e46c4b39aaed1730d497614
Saiyannaing23 <pesilay2372002@gmail.com>
2023-05-29 00:04:42 +0630

    Decorator

---
+++ decoration.py
@@ -0,0 +1,14 @@
+def greet(fun):
+    def wrapper(name):
+        #before
+        print("Hello")
+        fun(name)
+        #after
+        print("Good Bye")
+    
+    return wrapper
+@greet
+def sayName(name):
+    print(name)
+
+sayName("Yan Naing")
